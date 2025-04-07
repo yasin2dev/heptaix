5521b16541e46cfdd65c75eef2ecaa704add25a6
yasin2dev <yasin.bozkurt.n@gmail.com>
2025-04-07 21:27:50 +0300

    feat(navbar): navbar güncellendi

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -7,8 +7,7 @@ export default function Navbar() {
             <nav className="bg-white border-gray-200 dark:bg-gray-900">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl p-4">
                     <Link href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-                        <img src="https://www.epati.com.tr/assets/images/tr/_logos/logo/epati-logo-beyaz.png" className="h-8" alt="To-doApp Logo" />
-                        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">To-Do App</span>
+                        <span className="self-center text-2xl whitespace-nowrap dark:text-white text-amber-600 font-bold italic">Heptaix</span>
                     </Link>
                     <div className="flex items-center space-x-6 rtl:space-x-reverse">
                         <Link href="/login" className="text-sm  text-amber-600 dark:text-amber-500 hover:underline">Login</Link>
