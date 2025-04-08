3d5a71b5c65510b6294b7b2bb3cb8599fa1c54c7
yasin2dev <yasin.bozkurt.n@gmail.com>
2025-04-08 22:32:00 +0300

    feat(server/user): email alanı için zod doğrulaması yapıldı.

--- server/api/users/user.ts
+++ server/api/users/user.ts
@@ -5,17 +5,27 @@ import { listUsers } from '../../db/user/list';
 import { createUser } from '../../db/user/create';
 import { getUser } from '../../db/user/get';
 import { zRegisterUser } from '../../types';
+import { ZodError } from 'zod';
 
 const userRoute = Router()
 
 userRoute.post('/register', async (req: Request, res: Response) => {
-    const { name, surname, username, email, password } = req.body;
-    if (!req.body) res.status(400).send("Bad Request");
-    const hashedPassword: string = await bcrypt.hash(password, 12);
-    // TODO: Declared but never used. Zod validation system messages added. eg. invalid email
-    const validation = zRegisterUser.parse({ name: name, surname: surname, username: username, email: email, hash: hashedPassword });
-    createUser(name, surname, username, email, hashedPassword);
-    res.status(201).send("Register Successful.");
+    try {
+        const { name, surname, username, email, password } = req.body;
+        if (!name && !surname && !username && !email && !password) res.status(400).send("Bad Request");
+        const hashedPassword: string = await bcrypt.hash(password, 12);
+        // TODO: Declared but never used. Zod validation system messages added. eg. invalid email
+        const validation = zRegisterUser.parse({ name: name, surname: surname, username: username, email: email, hash: hashedPassword });
+        console.log(validation.email);
+        createUser(name, surname, username, email, hashedPassword).then(() => res.status(201).send("Register Successful."));
+    } catch (e) {
+        if (e instanceof ZodError) {
+            const emailError = e.issues.filter((issue) => issue.message === "Invalid email");
+            if (emailError.length > 0) {
+                res.send("Invalid email format.");
+            }
+        }
+    }
 })
 
 userRoute.post('/login', async (req: Request, res: Response) => {
@@ -29,7 +39,7 @@ userRoute.post('/login', async (req: Request, res: Response) => {
                 if (compare) {
                     const jwtSecret = process.env.JWT_SECRET_KEY;
                     if (!jwtSecret) throw new Error("JWT Secret Key is not defined in .env file");
-                    const token = jwt.sign(u, jwtSecret, {expiresIn: '1h'});
+                    const token = jwt.sign(u, jwtSecret, { expiresIn: '1h' });
                     res.status(200).json({ id: u.id, name: u.name, surname: u.surname, username: u.username, email: u.email, token: token });
                 } else {
                     res.status(401).send("User credentials is not true")
