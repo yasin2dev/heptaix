e0870567db1c49d06c53e16592f1f754f8e35477
yasin2dev <yasin.bozkurt.n@gmail.com>
2025-04-10 01:17:50 +0300

    refactor: lazy load for navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Navbar from "./components/Navbar";
-import { ThemeProvider } from "./components/theme-provider";
+
+import dynamic from "next/dynamic";
+
+const Navbar = dynamic(() => import('./components/Navbar'), {ssr: !!false});
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
