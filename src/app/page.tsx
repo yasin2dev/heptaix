2a21aadb05ef7e92bca528efe96b7bb322ac2669
yasin2dev <yasin.bozkurt.n@gmail.com>
2025-04-11 01:55:01 +0300

    refactor(homepage): handleTodo function updates. * Rename states * example view of todo added * minor code edits

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,56 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
-import { Card, CardContent } from "@/components/ui/card";
-import axios from "axios";
-import Image from "next/image";
 import { useEffect, useState } from "react";
 
+import axios from "axios";
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import UnauthorizatedScreen from "./components/UnauthorizatedScreen";
+import { Todo } from "../../server/types";
+
 export default function Home() {
-  const [logged, setLogged] = useState<boolean>(false);
-  const [todo, setTodo] = useState<[] | null | any>([]);
+  const [authorizated, setAuthorizated] = useState<boolean>(false);
+  const [todos, setTodos] = useState<[]>([]);
 
   useEffect(() => {
     const token = localStorage.getItem("JWT_TOKEN");
     if (token) {
-      setLogged(true);
+      setAuthorizated(true);
     }
+    handleTodo();
   }, [])
 
   return (
     <>
       {
-        !logged
+        !authorizated
           ?
           <UnauthorizatedScreen />
           :
+          <div className="container flex justify-between m-auto space-x-2 rtl:space-x-reverse">
+            {todos.map((a: Todo) => (
+              <Card key={a.id} className="w-[28rem] mt-8">
+                <CardHeader>
+                  <CardTitle>{a.title}</CardTitle>
+                  <CardDescription>{a.description}</CardDescription>
+                </CardHeader>
                 <CardContent>
-                  <p>{a?.title}</p>
-                  <p>{a?.description}</p>
+                  <p>EXAMPLE TODO CONTENT</p>
                 </CardContent>
+                <CardFooter>
+                  <p className="text-xs">01/01/1970</p>
+                </CardFooter>
               </Card>
             ))
             }
-            <Button onClick={handleTodo}>Get Todo</Button>
           </div>
       }
     </>
   );
 
   async function handleTodo() {
-    await axios.get("http://localhost:4001/api/todo/lorem").then((data) => {
-      setTodo(data.data)
+    await axios.get("http://localhost:4001/api/todo/lorem").then((result) => {
+      setTodos(result.data)
     })
   }
 }
