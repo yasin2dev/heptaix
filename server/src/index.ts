4463d493dddb1ae02bc007b3789e4696edbc0e0a
Yasin Bozkurt <yasin.bozkurt.n@gmail.com>
2025-04-25 01:54:01 +0300

    fix(issue-8): Max JSON payload size set to 100kb

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,4 +1,5 @@
 import express, {Request, Response} from 'express'; 
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
@@ -9,7 +10,7 @@ const app = express();
 dotenv.config();
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json({ limit: '100kb' }));
 app.use('/api/user', userRoute)
 app.use('/api/todo', todoRouter)
 
