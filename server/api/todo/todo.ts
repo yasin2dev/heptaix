0c135d177a5d704b948c9fdfae1e15b1e11cb780
yasin2dev <yasin.bozkurt.n@gmail.com>
2025-08-10 00:42:19 +0300

    feat(todo): implement encryption and decryption for todo items

--- server/api/todo/todo.ts
+++ server/api/todo/todo.ts
@@ -3,6 +3,8 @@ import { randomUUID } from "crypto";
 
 import { createTodo, listTodos } from "@server/db";
 import { verifyToken } from "../../auth/index";
+import { decrypt, encrypt } from "@common/helpers";
+import { Todo } from "@server/types";
 
 const todoRouter = Router();
 
@@ -12,15 +14,23 @@ todoRouter.get("/list", (req: Request, res: Response) => {
   if (!jwtResult) {
     res.status(500).send("Internal Server Error");
   } else if (jwtResult.success) {
-    listTodos(jwtResult.payload.id).then((result) => {
-      res.status(200).json(result)
+    listTodos(jwtResult.payload.id).then(async (result) => {
+      const decryptedTodos = await Promise.all(
+        result.map(async (todo) => ({
+          ...todo,
+          title: await decrypt(todo.title),
+          textContent: await decrypt(todo.textContent),
+          description: await decrypt(todo.description)
+        }))
+      );
+      res.status(200).json(decryptedTodos);
     })
   } else {
     res.status(400).send(jwtResult.error)
   }
 })
 
-todoRouter.post("/create", (req: Request, res: Response) => {
+todoRouter.post("/create", async (req: Request, res: Response) => {
   const authHeader = req.headers['authorization'];
   const jwtResult = verifyToken(authHeader);
 
@@ -31,7 +41,13 @@ todoRouter.post("/create", (req: Request, res: Response) => {
     res.status(500).send("Internal Server Error");
   } else if (jwtResult.success) {
     const todoId = randomUUID();
-    createTodo(todoId, title, textContent, jwtResult.payload.id, createdAt, description);
+    // Encrypt todo title, text content and description
+    // @TODO - better encryption syntax
+    const encTitle = await encrypt(title);
+    const encContent = await encrypt(textContent);
+    const encDescription = await encrypt(description);
+
+    createTodo(todoId, encTitle, encContent, jwtResult.payload.id, createdAt, encDescription);
     res.status(201).json({ todoId, title, description, textContent, createdAt });
   } else {
     res.status(406).send(jwtResult.error)
